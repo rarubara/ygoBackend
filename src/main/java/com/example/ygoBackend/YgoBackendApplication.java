f2235f2b2c3fed1cffc0321dbad6370332f18966
ralvarado <ruben.alvarado@pucp.pe>
2024-11-24 18:23:27 -0500

    initial commit

---
+++ src/main/java/com/example/ygoBackend/YgoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ygoBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YgoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YgoBackendApplication.class, args);
+	}
+
+}
